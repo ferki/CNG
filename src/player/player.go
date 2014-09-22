061eb5c9434ff29731799d4aa633e45e9c6199e4
Pisush <natalie@adjust.com>
2014-09-22 18:50:39 +0100

    added raise

--- src/player/player.go
+++ src/player/player.go
@@ -14,6 +14,10 @@ func IsPair(c1 *Card, c2 *Card) bool{
 	return c1.Rank==c2.Rank;
 }
 
+func Raise(state *GameState, value float) int {
+	return int(value*state.Pot);
+}
+
 func Version() string {
     return VERSION
 }
