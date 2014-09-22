c624635d95e96652444c957deef9daf9f3bb8da8
Pisush <natalie@adjust.com>
2014-09-22 19:09:20 +0100

    added wrapper function to compare is_pair

--- src/player/player.go
+++ src/player/player.go
@@ -10,8 +10,12 @@ func Showdown(state *GameState) {
     
 }
 
-func IsPair(c1 *Card, c2 *Card) bool{
-	return c1.Rank==c2.Rank;
+func IsPair_helper(c1 *Card, c2 *Card) bool {
+	return (c1.Rank==c2.Rank);
+}
+
+func IsPair(p Player) bool {
+	return (IsPair_helper(p.HoleCards[0], p.HoleCards[1]));
 }
 
 func Raise(state *GameState, value float) int {
